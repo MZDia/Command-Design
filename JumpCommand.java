1ef031ff7ae75c6cdc6f24c5ef409a73b969030d
Samuel Godfrey <sjg6@email.sc.edu>
2022-02-24 16:01:17 -0500

    Updated Player class jump & fire methods (haven't been tested yet), wrote JumpCommand class

--- JumpCommand.java
+++ JumpCommand.java
@@ -1,20 +1,27 @@
 /**
- * 
+ * Commands player character to jump
  * @author Mia Dia, Mark Valentino, Samuel Godfrey, Abhinav Myadala
  */
 public class JumpCommand implements Command {
     /**
-     * 
+     * Declaration of player to be used within this class
+     */
+    private Player player;
+
+    /**
+     * Constructor, initializes declared player outside of class
+     * to player passed through parameter, then calls execute.
      * @param player
      */
     public JumpCommand (Player player) {
-        ;
+        this.player = player;
+        execute();
     }
 
     /**
-     * 
+     * Calls jump method in class Player for object player.
      */
     public void execute () {
-        ;
+        player.jump();
     }
 }
--- Player.java
+++ Player.java
@@ -1,3 +1,4 @@
+import java.io.*;
 /**
  * 
  * @author Mia Dia, Mark Valentino, Samuel Godfrey, Abhinav Myadala
@@ -11,17 +12,67 @@ public class Player {
     }
 
     /**
-     * 
+     * Reads text file named "jump.txt", goes through lines in file,
+     * and prints out every 6 lines of code before replacing the output
+     * with the next 6 lines every second.
      */
     public void jump () {
-        ;
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader("jump.txt"));
+            String line = reader.readLine();
+            int i = 0;
+            while (line != null) {
+                if (i == 6) {
+                    i = 0;  // resets i to 0 if 6 lines have been output already
+                    try {
+                        Thread.sleep(1000);  // program sleeps for one second
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.print("\033[H\033[2J");  // ANSI code to clear screen
+                    System.out.flush(); // flushes out remaining bytes
+                } else {
+                    System.out.println(line);
+                    i++;
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
-     * 
+     * Reads text file named "fire.txt", goes through lines in file,
+     * and prints out every 3 lines of code before replacing the output
+     * with the next 3 lines every second.
      */
     public void fire () {
-        ;
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader("fire.txt"));
+            String line = reader.readLine();
+            int i = 0;
+            while (line != null) {
+                if (i == 3) {
+                    i = 0;  // resets i to 0 if 3 lines have been output already
+                    try {
+                        Thread.sleep(1000);  // program sleeps for one second
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.print("\033[H\033[2J");  // ANSI code to clear screen
+                    System.out.flush();  // flushes out remaining bytes
+                } else {
+                    System.out.println(line);
+                    i++;
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
