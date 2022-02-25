ec0f0f78c879e9222fa5768ddf3a5c4c3d4dd167
M-Valentino <mike9377mike9377@gmail.com>
2022-02-25 17:06:04 -0500

    Small edit to QuitCommand.java. Code has been compiled.

--- QuitCommand.java
+++ QuitCommand.java
@@ -8,13 +8,14 @@ public class QuitCommand implements Command {
      * @param player
      */
     public QuitCommand (Player player) {
-        ;
+        //this.player = player;
+        //exectute();
     }
 
     /**
-     * 
+     * Method to call the quit() method in the player class
      */
     public void execute () {
-        ;
+        //player.quit();
     }
 }
