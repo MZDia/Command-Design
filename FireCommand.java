c92c079a6cecda776a4e0f1a0cabf4718ba055e4
Abhinav110102 <abhinavmyadala@gmail.com>
2022-02-25 12:39:43 -0500

    Firecommand

--- FireCommand.java
+++ FireCommand.java
@@ -3,18 +3,23 @@
  * @author Mia Dia, Mark Valentino, Samuel Godfrey, Abhinav Myadala
  */
 public class FireCommand implements Command {
+   /**
+    * 
+    */ 
+    private Player player;
     /**
      * 
      * @param player
      */
     public FireCommand (Player player) {
-        ;
+       this.player = player;
+       execute();
     }
 
     /**
      * 
      */
     public void execute () {
-        ;
+        player.fire();
     }
 }
