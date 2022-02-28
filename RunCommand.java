f31fbd04b7aeec608271a2e3c50d4002b7c875e7
Abhinav110102 <abhinavmyadala@gmail.com>
2022-02-27 19:23:08 -0500

    Comments

--- RunCommand.java
+++ RunCommand.java
@@ -1,10 +1,11 @@
 /**
- * 
+ * Commands player character to run
  * @author Mia Dia, Mark Valentino, Samuel Godfrey, Abhinav Myadala
  */
 public class RunCommand implements Command {
+    
     /**
-     * 
+     * Constructor, initializes declared player outside of class to player passed through parameter, then calls execute.
      * @param player
      */
     public RunCommand (Player player) {
@@ -12,7 +13,7 @@ public class RunCommand implements Command {
     }
 
     /**
-     * 
+     * Calls run method in class Player for object player.
      */
     public void execute () {
         ;
