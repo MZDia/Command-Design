521b6d5d40ad2ee71960e06ef47c5b8448c2fbd5
Abhinav110102 <abhinavmyadala@gmail.com>
2022-02-27 19:35:37 -0500

    Merge branch 'main' of https://github.com/MZDia/Command-Design

--- RunCommand.java
+++ RunCommand.java
@@ -1,11 +1,6 @@
 /**
-<<<<<<< HEAD
- * Commands player character to run
- * @author Mia Dia, Mark Valentino, Samuel Godfrey, Abhinav Myadala
-=======
  * Creates an instance of the Run Commands
  * @author Mia Dia(main) Samuel Godfrey(skeleton) 
->>>>>>> 8b983ce2f2e0e019c057e3fd5beaf6ec22a6ebb9
  */
 
 public class RunCommand implements Command {
@@ -25,11 +20,7 @@ public class RunCommand implements Command {
     }
 
     /**
-<<<<<<< HEAD
-     * Calls run method in class Player for object player.
-=======
      * Executes the player's run command
->>>>>>> 8b983ce2f2e0e019c057e3fd5beaf6ec22a6ebb9
      */
     public void execute () {
         this.player.runForward();
